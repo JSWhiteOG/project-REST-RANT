d986436a36bdc095407878d272cb53c44f72d017
James Swinson <jswinsonwhite@gmail.com>
2023-08-27 12:00:19 -0700

    added bootstrap

--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,12 @@ function home () {
     <Def>
         <main>
             <h1>HOME</h1>
+            <div>
+              <img src="/images/thumbs.jpg" alt="Thumbs"/>
+              <div>
+                Photo by <a href="https://www.freepik.com/author/valuavitaly">Valuavitaly</a> on <a href="https://www.freepik.com/">Freepik</a>
+              </div>
+            </div>
             <a href="/places">
   <button className="btn-primary">Places Page</button>
 </a>
