357c2323cba26e41f05d9669dd475836087b37c7
James Swinson <jswinsonwhite@gmail.com>
2023-08-29 19:50:58 -0700

    Navbar with style

--- controllers/places.js
+++ controllers/places.js
@@ -3,16 +3,12 @@ const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
   res.render('places/index', { places })
-
 })
 
 router.get('/new', (req, res) => {
-
-res.render('places/new')
+   res.render('places/new')
 })
  
-
-
 router.post('/', (req, res) => {
   
   if (!req.body.pic) {
