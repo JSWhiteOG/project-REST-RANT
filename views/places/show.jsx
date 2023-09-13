0bcd8e42caa931abdaa0f491b4c2ff398255d37d
James Swinson <jswinsonwhite@gmail.com>
2023-09-12 19:49:49 -0700

    added vercel(?)

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,32 +2,6 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
-    let comments = (
-     <h3 className="inactive">
-      No Comments yet!
-     </h3>
-    )
-    let rating = (
-        <h3 className="inactive">
-          Not yet rated
-        </h3>
-      )
-      if (data.place.comments.length) {
-        let sumRatings = data.place.comments.reduce((tot, c) => {
-          return tot + c.stars
-        }, 0)
-        let averageRating = Math.round(sumRatings / data.place.comments.length)
-        let stars = ''
-        for (let i = 0; i < averageRating; i++) {
-          stars += '* '
-        }
-        rating = (
-          <h3>
-            {stars} stars
-          </h3>
-        )
-      
-        comments = data.place.comments.map(c => {
           return (
             
           <div className="border">
@@ -45,8 +19,8 @@ function show (data) {
                  </div>
           
         )
-      })
-    }
+      }
+    
     return (
         <Def>
           <main>
@@ -137,7 +111,7 @@ function show (data) {
           </main>
         </Def>
     )
-}
+
 
 module.exports = show
     
